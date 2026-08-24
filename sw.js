d27fb36b68dee2a32cadd8885c9d9c68fb1a4a30
acardewe <196703799+acardewe@users.noreply.github.com>
2026-08-24 16:12:06 -0400

    Actualizar guía Japón 2026

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "japon-2026-43e3659fb518";
+const CACHE = "japon-2026-b80fb38496de";
 const BASE = location.pathname.replace(/\/[^/]*$/, "/");
 const RECURSOS = [BASE, BASE + "index.html", BASE + "manifest.json",
                   BASE + "icono-192.png", BASE + "icono-512.png",
