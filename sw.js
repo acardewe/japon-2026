48b6e4c4cc6e3428206ad7a29532345c3dfb2535
acardewe <196703799+acardewe@users.noreply.github.com>
2026-09-04 20:15:22 -0400

    Mejorar navegación, itinerario y mapas del viaje

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "japon-2026-ecdce3817866";
+const CACHE = "japon-2026-0edfde5be19c";
 const BASE = location.pathname.replace(/\/[^/]*$/, "/");
 const RECURSOS = [BASE, BASE + "index.html", BASE + "manifest.json",
                   BASE + "icono-192.png", BASE + "icono-512.png",
